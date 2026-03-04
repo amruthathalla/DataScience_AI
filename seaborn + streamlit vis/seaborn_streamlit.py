da0e635090d45011a8207e242213f4ba2662926b
Amrutha Thalla <thallaamrutha5@gmail.com>
2026-03-04 18:04:39 +0530

    Update notebook before merge

--- seaborn + streamlit vis/seaborn_streamlit.py
+++ seaborn + streamlit vis/seaborn_streamlit.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
-import pandas as pd
 
 st.title("Seaborn Visualization Dashboard on Tips Dataset By Amrutha Thalla")
 
